b5601ad43178563ca2bd6d27488756aa72a68f4d
root <root@DESKTOP-482253N.localdomain>
2021-10-11 21:52:56 +1100

    assignment passed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,22 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+    # # @post.update(post_params)
+    # @post = Post.new(post_params)
+
+    # if @post.valid?
+    #   @post.update(post_params)
+    #   redirect_to post_path(@post)
+    # else
+    #   render :edit
+    # end
+
   end
 
   private
